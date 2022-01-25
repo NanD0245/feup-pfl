552f068dfb0003ebf2435dc29639adb4cfaadc3d
NanD0245 <fernandoluisrego@gmail.com>
2022-01-25 23:38:32 +0000

    fix find_flights_breadth

--- PROLOG/TP4/ex4.pl
+++ PROLOG/TP4/ex4.pl
@@ -1,3 +1,9 @@
+% utils
+invert(L1,L2) :- reverse(L1,L2,[]).
+
+reverse([],L2,L2).
+reverse([H|T],L2,Acc) :- reverse(T,L2,[H|Acc]).
+
 :- dynamic male/1, female/1.
 
 % EX1
@@ -44,7 +50,7 @@ flight(madrid, paris, iberia, ib3444, 1640, 125).
 flight(madrid, london, iberia, ib3166, 1550, 145).
 flight(london, madrid, iberia, ib3163, 1030, 140).
 flight(porto, frankfurt, lufthansa, lh1177, 1230, 165).
-flight(paris, ola, lufthansa, lh1177, 1230, 165).
+flight(paris, ola, lufthansa, lh1111, 1111, 111).
 
 % a)
 get_all_nodes(L) :- setof(Orig, Dest^Comp^Code^Hour^Dur^flight(Orig,Dest,Comp,Code,Hour,Dur),L).
@@ -66,24 +72,30 @@ get_most_common([C-N|T], Num, Acc, Comp) :-  (N > Num ->
                                         ).
 
 % c)
-find_flights_depth(Origin,Dest,Flights) :- find_flights_depth(Origin,Dest,[Origin],Flights).
+find_flights(Origin,Dest,Flights) :- find_flights(Origin,Dest,[Origin],Flights).
 
-find_flights_depth(Origin,Dest,Acc,Flights) :-  flight(Origin,Dest,_,_,_,_), append(Acc,[Dest],Flights).
-find_flights_depth(Origin,Dest,Acc,Flights) :-  flight(Origin,Mid,_,_,_,_),
-                                                \+ member(Mid, Acc),
-                                                append(Acc,[Mid],New),
-                                                find_flights_depth(Mid,Dest,New,Flights).
+find_flights(Origin,Dest,Acc,Flights) :-    flight(Origin,Dest,_,_,_,_), append(Acc,[Dest],Flights).
+find_flights(Origin,Dest,Acc,Flights) :-    flight(Origin,Mid,_,_,_,_),
+                                            \+ member(Mid, Acc),
+                                            append(Acc,[Mid],New),
+                                            find_flights(Mid,Dest,New,Flights).
 
-% d) 
-find_flights_breadth(S, F, Path):- find_flights_breadth([S], F, [],Path). 
+% d)    
+find_flights_breadth(Origin,Dest,Flights) :- find_flights_breadth([Origin],Dest,[Origin],[[Origin]],Flights).
 
-find_flights_breadth([F|_], F, V, [F]) :- !.
-find_flights_breadth([S|R], F, V,Path) :-   findall(N,(flight(S,N,_,_,_,_), \+ member(N, V), \+ member(N, [S|R])),  L),
-                                    append(R, L, NR),
-                                    find_flights_breadth(NR, F, [S|V], OLDPath), !,
-                                    append([H],T,OLDPath), 
-                                    (flight(S,H,_,_,_,_) -> % not 100% sure about this solution
-                                        append([S],OLDPath,Path)
-                                    ;
-                                        Path = OLDPath
-                                    ).
+find_flights_breadth([Dest | _], Dest, _V, Acc, Flights) :- append(_,[[Dest|T]|_],Acc), append([],[Dest|T],P), invert(P,Flights).
+
+find_flights_breadth([S | R], F, V, Acc, Flights) :-
+            findall(N, (flight(S,N,_,_,_,_), \+ member(N,V), \+ member(N, [S|R])),L),
+            append_flights(L,S,Acc,[],New),
+            append(R, L, NR),
+            find_flights_breadth(NR,F,[S|V],New,Flights).
+
+append_flights([],_,[],New,New).
+append_flights(_L,S,[[H|T]|TT],Acc,New) :- \+ equal(H,S), append(Acc, [[H|T]], NAcc), append_flights(_L,S,TT,NAcc,New).
+append_flights([],S,[_|TT],Acc,New) :- append_flights([],S,TT,Acc,New).
+append_flights([A|AA],S,[H|TT],Acc,New) :-  append([A],H,F),
+                                            append(Acc,[F],NAcc),
+                                            append_flights(AA,S,[H|TT],NAcc,New).
+
+equal(X,X).
