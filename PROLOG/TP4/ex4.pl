598b158a157d03aa86ec54ec3e17cd4dc5b29dc6
NanD0245 <fernandoluisrego@gmail.com>
2022-01-26 00:27:16 +0000

    continue ex2

--- PROLOG/TP4/ex4.pl
+++ PROLOG/TP4/ex4.pl
@@ -4,6 +4,8 @@ invert(L1,L2) :- reverse(L1,L2,[]).
 reverse([],L2,L2).
 reverse([H|T],L2,Acc) :- reverse(T,L2,[H|Acc]).
 
+equal(X,X).
+
 :- dynamic male/1, female/1.
 
 % EX1
@@ -74,16 +76,21 @@ get_most_common([C-N|T], Num, Acc, Comp) :-  (N > Num ->
 % c)
 find_flights(Origin,Dest,Flights) :- find_flights(Origin,Dest,[Origin],Flights).
 
-find_flights(Origin,Dest,Acc,Flights) :-    flight(Origin,Dest,_,_,_,_), append(Acc,[Dest],Flights).
+find_flights(Origin,Dest,Acc,Flights) :-    flight(Origin,Dest,_,_,_,_), append(Acc,[Dest],[O|F]),get_codes(O,F,[],Flights).
 find_flights(Origin,Dest,Acc,Flights) :-    flight(Origin,Mid,_,_,_,_),
                                             \+ member(Mid, Acc),
                                             append(Acc,[Mid],New),
                                             find_flights(Mid,Dest,New,Flights).
 
+get_codes(_,[],Flights,Flights).
+get_codes(Origin,[H|T],Acc,Flights) :-  flight(Origin,H,_,Code,_,_),
+                                        append(Acc,[Code],New),
+                                        get_codes(H,T,New,Flights).
+
 % d)    
 find_flights_breadth(Origin,Dest,Flights) :- find_flights_breadth([Origin],Dest,[Origin],[[Origin]],Flights).
 
-find_flights_breadth([Dest | _], Dest, _V, Acc, Flights) :- append(_,[[Dest|T]|_],Acc), append([],[Dest|T],P), invert(P,Flights).
+find_flights_breadth([Dest | _], Dest, _V, Acc, Flights) :- append(_,[[Dest|T]|_],Acc), append([],[Dest|T],P), invert(P,[O|F]), get_codes(O,F,[],Flights).
 
 find_flights_breadth([S | R], F, V, Acc, Flights) :-
             findall(N, (flight(S,N,_,_,_,_), \+ member(N,V), \+ member(N, [S|R])),L),
@@ -98,4 +105,13 @@ append_flights([A|AA],S,[H|TT],Acc,New) :-  append([A],H,F),
                                             append(Acc,[F],NAcc),
                                             append_flights(AA,S,[H|TT],NAcc,New).
 
-equal(X,X).
+% e)
+find_all_flights(Origin, Dest, ListOfFlights) :- setof(Flights,Origin^Dest^find_flights(Origin,Dest,Flights),ListOfFlights).
+
+% f)
+find_flights_least_stops(Origins, Destinations, ListOfFlights) :- find_flights_least_stops(Origins, Destinations, [], ListOfFlights).
+
+find_flights_least_stops([], [], ListOfFlights, ListOfFlights).
+find_flights_least_stops([Origin|OT], [Dest|DT], Acc, ListOfFlights) :- find_flights_breadth(Origin, Dest, L),
+                                                                        append(Acc,[L],New),
+                                                                        find_flights_least_stops(OT,DT,New,ListOfFlights).
